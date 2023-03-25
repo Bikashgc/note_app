994e2b4fa64fa101e885f89ad76e4e15a2937070
Bikash GC <bikashgc937@gmail.com>
2023-03-25 22:35:34 +0545

    Completed UI of homepage where added or saved note is viewed

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      body: getNoteListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('add note button clicked');
+        },
+        tooltip: 'Add a note',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  ListView getNoteListView() {
+    TextStyle? titleStyle = Theme.of(context).textTheme.headline6;
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.red,
+            ),
+            title: Text(
+              'Title of note will appear here',
+              style: titleStyle,
+            ),
+            subtitle: Text('subtitle will appear here'),
+            trailing: Icon(
+              Icons.delete,
+              color: Colors.grey,
+            ),
+            onTap: () {
+              debugPrint("List Tile clicked");
+            },
+          ),
+        );
+      },
+    );
+  }
+}
