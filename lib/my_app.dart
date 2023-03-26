c5e1acf5c71d971a3f104b02ed37e9e28be32b29
Bikash GC <bikashgc937@gmail.com>
2023-03-26 18:22:58 +0545

    Completed UI of Add Note screen from where users can add or save notes.

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/screens/add_note.dart';
 
 import 'screens/home_page.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: HomePage(),
+      home: AddNote(),
     );
   }
 }
